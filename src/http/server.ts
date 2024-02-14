8d91acc77cfd82b52269544dfc008ad5a510640d
ezequiel avell <e9640879@gmail.com>
2024-02-14 16:39:21 -0300

    aula 3

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,20 +1,26 @@
-import fastify from 'fastify'
-import { createPoll } from './routes/create-poll'
-import { getPoll } from './routes/get-poll'
-import { voteOnPoll } from './routes/vote-on-poll'
-import cookie from '@fastify/cookie'
+import fastify from "fastify";
+import cookie from "@fastify/cookie";
+import websocket from "@fastify/websocket";
+import { createPoll } from "./routes/create-poll";
+import { getPoll } from "./routes/get-poll";
+import { voteOnPoll } from "./routes/vote-on-poll";
+import { pollResults } from "./ws/poll-results";
 
 const app = fastify()
 
 app.register(cookie, {
-  secret : "polls-app-nlw-2024-",
-  hook : "onRequest",
+  secret: "polls-app-nlw",
+  hook: 'onRequest',
 })
+
+app.register(websocket)
 
 app.register(createPoll)
 app.register(getPoll)
 app.register(voteOnPoll)
 
-app.listen({ port : 3333}).then(() => {
-  console.log('http server running')
+app.register(pollResults)
+
+app.listen({ port: 3333 }).then(() => {
+  console.log("HTTP server running!")
 })
